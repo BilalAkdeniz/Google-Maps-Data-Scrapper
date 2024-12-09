9c6a74a1051d7119e3bb0a7b6af87caacb3aafd1
BilalAkdeniz <bilalakdeniz002@gmail.com>
2024-12-09 15:09:52 +0300

    Google-Maps-Data-Scrapper

---
+++ GoogleMapsVeriÇekme/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GoogleMapsVeriÇekme
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtKeywords = new Siticone.Desktop.UI.WinForms.SiticoneTextBox();
+            this.btnStart = new Siticone.Desktop.UI.WinForms.SiticoneGradientButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtKeywords
+            // 
+            this.txtKeywords.BorderColor = System.Drawing.Color.White;
+            this.txtKeywords.BorderRadius = 25;
+            this.txtKeywords.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtKeywords.DefaultText = "";
+            this.txtKeywords.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtKeywords.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtKeywords.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtKeywords.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtKeywords.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.txtKeywords.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.txtKeywords, "txtKeywords");
+            this.txtKeywords.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtKeywords.Name = "txtKeywords";
+            this.txtKeywords.PasswordChar = '\0';
+            this.txtKeywords.PlaceholderText = "Anahtar kelimeleri giriniz...";
+            this.txtKeywords.SelectedText = "";
+            // 
+            // btnStart
+            // 
+            this.btnStart.BorderRadius = 25;
+            this.btnStart.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnStart.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnStart.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStart.DisabledState.FillColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnStart.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.ForeColor = System.Drawing.Color.White;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::GoogleMapsVeriÇekme.Properties.Resources.dark_mode;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnStart;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(54)))), ((int)(((byte)(54)))), ((int)(((byte)(54)))));
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtKeywords);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Siticone.Desktop.UI.WinForms.SiticoneTextBox txtKeywords;
+        private Siticone.Desktop.UI.WinForms.SiticoneGradientButton btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ GoogleMapsVeriÇekme/Form1.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using ClosedXML.Excel;
+using System.IO;
+using WebDriverManager.DriverConfigs.Impl;
+using OpenQA.Selenium.Support.UI;
+using System.Threading;
+using WebDriverManager;
+
+namespace GoogleMapsVeriÇekme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string keywords = txtKeywords.Text.Trim();
+
+            if (string.IsNullOrEmpty(keywords))
+            {
+                MessageBox.Show("Lütfen aramak istediğiniz anahtar kelimeleri girin.");
+                return;
+            }
+
+            try
+            {
+                List<BusinessInfo> businesses = ScrapeGoogleMaps(keywords);
+
+                // Verileri Excele kaydetme
+                string excelPath = SaveToExcel(businesses);
+
+                // Exceli başlatma
+                System.Diagnostics.Process.Start("explorer.exe", excelPath);
+
+                MessageBox.Show("Veriler başarıyla kaydedildi ve Excel dosyası açıldı!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Bir hata oluştu: {ex.Message}");
+            }
+        }
+
+        private List<BusinessInfo> ScrapeGoogleMaps(string keywords)
+        {
+            List<BusinessInfo> businessList = new List<BusinessInfo>();
+            HashSet<string> collectedBusinessUrls = new HashSet<string>();
+
+            new DriverManager().SetUpDriver(new ChromeConfig());
+
+            var options = new ChromeOptions();
+            options.AddArgument("--no-sandbox");
+            options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("--disable-gpu");
+            options.AddArgument("--start-maximized");
+            options.AddArgument("--lang=en");
+            options.AddArgument("user-agent=<your-agent-string>");
+
+            using (IWebDriver driver = new ChromeDriver(options))
+            {
+                WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+                driver.Navigate().GoToUrl("https://www.google.com/maps?hl=en"); 
+                var searchBox = driver.FindElement(By.Id("searchboxinput"));
+                searchBox.SendKeys(keywords);
+                searchBox.SendKeys(OpenQA.Selenium.Keys.Enter);
+
+                Thread.Sleep(5000); // Sayfa yüklenmeden işleme başlıyorsa burdaki süreyi arttır!!!
+
+                IWebElement scrollArea = driver.FindElement(By.XPath("//div[@role='feed']"));
+                IJavaScriptExecutor jsExecutor = (IJavaScriptExecutor)driver;
+
+                bool moreResultsAvailable = true;
+
+                while (moreResultsAvailable)
+                {
+                    int previousHeight = Convert.ToInt32(jsExecutor.ExecuteScript("return arguments[0].scrollHeight", scrollArea));
+                    jsExecutor.ExecuteScript("arguments[0].scrollTop = arguments[0].scrollHeight;", scrollArea);
+                    Thread.Sleep(3000);
+
+                    int newHeight = Convert.ToInt32(jsExecutor.ExecuteScript("return arguments[0].scrollHeight", scrollArea));
+                    if (newHeight == previousHeight)
+                    {
+                        moreResultsAvailable = false; 
+                    }
+
+                    var results = driver.FindElements(By.CssSelector(".Nv2PK"));
+                    foreach (var result in results)
+                    {
+                        try
+                        {
+
+                            string url = result.FindElement(By.CssSelector("a")).GetAttribute("href");
+                            if (collectedBusinessUrls.Contains(url))
+                            {
+                                continue; 
+                            }
+
+                            
+                            string name = "İsim bulunamadı.";
+                            string website = "Website bulunamadı.";
+                            string phone = "Telefon bilgisi bulunamadı.";
+
+                            result.Click();
+                            Thread.Sleep(3000);
+
+                            // İsim bilgisi
+                            try
+                            {
+                                name = driver.FindElement(By.XPath("(//div[@role='main' and @aria-label]//h1)[last()]")).Text;
+                            }
+                            catch (NoSuchElementException) { }
+
+                            // Web site bilgisi ve telefon bilgisi
+                            try
+                            {
+                                website = driver.FindElement(By.XPath("//a[contains(@aria-label, 'Website')]")).GetAttribute("href");
+                            }
+                            catch (NoSuchElementException) { }
+
+                            try
+                            {
+                                phone = driver.FindElement(By.XPath("//button[starts-with(@aria-label, 'Phone:')]")).Text;
+                            }
+                            catch (NoSuchElementException) { }
+
+                            businessList.Add(new BusinessInfo
+                            {
+                                Name = name,
+                                Link = url,
+                                Website = website,
+                                Phone = phone
+                            });
+
+                            
+                            collectedBusinessUrls.Add(url);
+                        }
+                        catch
+                        {
+                            continue; 
+                        }
+                    }
+                }
+                return businessList;
+            }
+        }
+
+
+
+        private string SaveToExcel(List<BusinessInfo> businesses)
+        {
+            string fileName = $"Businesses_{DateTime.Now:yyyyMMddHHmmss}.xlsx";
+            string filePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), fileName);
+
+            using (var workbook = new XLWorkbook())
+            {
+                var worksheet = workbook.Worksheets.Add("Businesses");
+                worksheet.Cell(1, 1).Value = "İsim";
+                worksheet.Cell(1, 2).Value = "Link";
+                worksheet.Cell(1, 3).Value = "Web Sitesi";
+                worksheet.Cell(1, 4).Value = "Telefon";
+
+                for (int i = 0; i < businesses.Count; i++)
+                {
+                    worksheet.Cell(i + 2, 1).Value = businesses[i].Name;
+                    worksheet.Cell(i + 2, 2).Value = businesses[i].Link;
+                    worksheet.Cell(i + 2, 3).Value = businesses[i].Website;
+                    worksheet.Cell(i + 2, 4).Value = businesses[i].Phone;
+                }
+                workbook.SaveAs(filePath);
+            }
+            return filePath;
+        }
+
+        private class BusinessInfo
+        {
+            public string Name { get; set; }
+            public string Link { get; set; }
+            public string Website { get; set; }
+            public string Phone { get; set; }
+        }
+
+        bool isWhite = true;
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            // Dark Mode Özellikleri
+            if(isWhite)
+            {
+                this.BackColor = Color.White;
+                label1.ForeColor = Color.Black;
+            }
+            else
+            {
+                this.BackColor = Color.FromArgb(54, 54, 54);
+                label1.ForeColor = Color.White;
+            } 
+            isWhite = !isWhite;
+            
+        }
+    }
+}
